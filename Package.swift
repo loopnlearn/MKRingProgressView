36904df6c42602360fc864432ea20db9dab92352
Pete Schwamb <pete@schwamb.net>
2020-11-26 13:08:19 -0600

    Compiling with SPM

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+     name: "MKRingProgressView",
+     platforms: [
+         .iOS(.v8)
+     ],
+     products: [
+         .library(name: "MKRingProgressView", targets: ["MKRingProgressView"])
+     ],
+     targets: [
+        .target(
+               name: "MKRingProgressView",
+               path: "MKRingProgressView"
+        )
+     ],
+     swiftLanguageVersions: [.v5]
+ )
